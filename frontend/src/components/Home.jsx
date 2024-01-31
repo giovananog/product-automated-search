43eef19fcdaac481a3354cd2a90f52b43989f896
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-31 10:14:50 -0300

    init: initial frontend structure started

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+function Home() {
+  return (
+    <div>
+    
+        <Header />
+        <p> App Home </p>
+        <Footer />
+
+    </div>
+  );
+}
+
+export default Home;
