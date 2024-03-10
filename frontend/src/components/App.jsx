cb00a4311812fe376e4a5c4c41820044e7f3a4d4
Giovana Nogueira <ongiovana2016@gmail.com>
2024-03-10 18:27:07 -0300

    feat: search field done

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -5,6 +5,7 @@ import {
     Route,
   } from "react-router-dom";
 import Home from "./Home";
+import Result from "./Result";
 
 
 function App() {
@@ -13,8 +14,8 @@ function App() {
       <div>
         <Routes>
 
-          <Route path="/" element={<Home/>}>
-          </Route>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/search/:product" element={<Result/>}></Route>
           
         </Routes>
 
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import GenericCard from "./Card";
+import SearchField from "./Search";
+
 
 
 function Home() {
@@ -16,7 +18,13 @@ function Home() {
             <GenericCard name='Store 2' image='images/Store 2.jpg'/>
             <GenericCard name='Store 3' image='images/Store 3.png'/>
           </div>
+
         </div>
+
+        <div className="search-field-div">
+          <SearchField />
+        </div>
+        
 
         <Footer />
 
