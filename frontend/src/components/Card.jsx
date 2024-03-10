62549ab249dca73fe0cecea18eb2ad61d8a3d7e7
Giovana Nogueira <ongiovana2016@gmail.com>
2024-03-10 17:43:34 -0300

    feat: card styles done

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function GenericCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={props.image}
+          alt={props.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.name}
+          </Typography>
+
+          {props.isProduct && 
+            <Typography variant="body2" color="text.secondary">
+              {props.description}
+            </Typography>
+          }
+
+        </CardContent>
+      </CardActionArea>
+
+      {props.isProduct && 
+      <CardActions>
+        <Button size="small" color="primary">
+          Link
+        </Button>
+      </CardActions>
+      }
+    </Card>
+  );
+}
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <div>
     
         <Header />
-        <p> App Home </p>
+        
         <Footer />
 
     </div>
